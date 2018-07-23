224840a968ba451c5340063bdcb2cdc90e1baa21
Juliet Urbina <julieturbina8@gmail.com>
2018-07-23 01:32:09 -0400

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Glam-Artist
+
+Add Landing Page
+
+Add Services Page
+    Cosmetic Procedures:
+        Procedures
+        Provider
+        Image
+
+Add Fashion Page
+    Fashion Designes
+
+Add Makeup Page
+    Makeup looks
+
+Create partials folder
+     Connect page to partial page to push procedure list.
+
+********Creating Services: create new Procedures/Services
+********Review = test failed
+
+
+
--- app.js
+++ app.js
@@ -14,9 +14,9 @@ mongoose.Promise = Promise;
 mongoose
   .connect('mongodb://localhost/glam-artist', {useMongoClient: true})
   .then(() => {
-    console.log('Connected to Mongo!')
+    console.log('Connected to Mongo!');
   }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 
 const app_name = require('./package.json').name;
@@ -27,7 +27,8 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
@@ -44,10 +45,75 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+hbs.registerPartials(__dirname + '/views/partials');
 
+// ============services ========== 
+
+const services = [
+  {
+    'name': 'Botox Injections', 
+    'provider': 'Jules', 
+    'photo': '../images/botox.png'
+    },
+    {
+      'name': 'Vampire Facials', 
+      'provider': 'Jules', 
+      'photo': '../images/vampire-facial-facelift.jpg'
+      },
+      {
+        'name': 'Facials', 
+        'provider': 'Jules', 
+        'photo': '../images/facials.jpg'
+        },
+];
+
+
+
+// =======  Get static files ===========
+
+//Get home page===
+app.get('/', (req, res, next) => {
+  res.render('index');
+});
+//Get services page===
+
+app.get('/services', (req, res, next) => {
+  res.render("services", {services:services}); 
+});
+
+app.post("/services", (req, res, next) => {
+  //get data from form and add to services array
+  const name = req.body.name;
+  const provider = req.body.provider;
+  const photo = req.body.photo;
+  const newServices = {name:name, provider:provider, photo:photo};
+  services.push(newServices);
+  //redirect back to services page
+  res.redirect("/services");
+});
+
+app.get("/services/new", (req, res) => {
+  res.render("newServices");
+});
+
+//Get fashion page===
+app.get('/fashion', (req, res, next) => {
+  res.render('fashion');
+});
+//Get makeup page===
+app.get('/makeup', (req, res, next) => {
+  res.render('makeup');
+});
+//To NOT add the layout to he home page===
+app.get('/index', (req, res, next) => {
+  let data = {
+    layout: false
+  };
+  res.render('teams', data);
+});
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Glam Artist - by Juliet Urbina';
 
 
 
